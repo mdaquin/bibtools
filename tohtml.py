05319bc98c693e42b05fadeed75c46a2db6e0299
matdaq <mathieu.daquin@insight-centre.org>
2020-01-31 10:55:20 +0000

    better handling of authors

--- tohtml.py
+++ tohtml.py
@@ -21,16 +21,25 @@ bib_data = parser.parse_file(sys.argv[1])
 
 # date (year and month)
 # then sort by date before doing anything else
-# authors should work better
 for key in bib_data.entries:
     entry = bib_data.entries[key]
     title = ""
     venue = ""
-    author = ""
+    st = '<span class="bib_authors">'    
     if "author" in entry.persons:
+        first = True
         for pers in entry.persons["author"]:
-            print(str(pers.first_names)+" "+str(pers.middle_names)+" "+str(pers.last_names))
-    print(entry.persons["author"])
+            if not first:
+                st = st + ', '
+            first = False
+            st = st + '<span class="bib_author">'
+            if len(pers.first_names) > 0:
+                st = st + str(pers.first_names[0])
+            if len(pers.middle_names) > 0:
+                st = st + " "+str(pers.middle_names[0])
+            if len(pers.last_names) > 0:
+                st = st + " " +str(pers.last_names[0].encode('utf-8'))
+            st = st + '</span>'            
     if "title" in entry.fields:
         title = entry.fields["title"]        
     if "author" in entry.fields:
@@ -44,8 +53,8 @@ for key in bib_data.entries:
     elif "publisher" in entry.fields:
         venue = entry.fields["publisher"]
     print('<p class="bib_entry" id="bib_'+key+'">')
-    print('span class="bib_title">'+title+'</span>')
-    print('<span class="bib_author">'+author+'</span>')
-    print('span class="bib_venue">'+venue+'</span>')
+    print('<span class="bib_title">'+title+'</span>')
+    print(st)
+    print('<span class="bib_venue">'+venue+'</span>')
     print('</p>')
     
