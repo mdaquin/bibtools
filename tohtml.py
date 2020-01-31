f4b67685899a7c4a250c4eed57a7862cf1539ede
matdaq <mathieu.daquin@insight-centre.org>
2020-01-31 17:01:11 +0000

    sorting in tohtml

--- tohtml.py
+++ tohtml.py
@@ -15,12 +15,10 @@ with open(sys.argv[1]) as f:
             print("Line not decoding: ")
             print(line)
 
-parser = bibtex.Parser()
+parser = bibtex.Parser(encoding="utf-8")
 bib_data = parser.parse_file(sys.argv[1])
+all_bibs = {}
 
-
-# date (year and month)
-# then sort by date before doing anything else
 for key in bib_data.entries:
     entry = bib_data.entries[key]
     title = ""
@@ -34,14 +32,14 @@ for key in bib_data.entries:
             first = False
             st = st + '<span class="bib_author">'
             if len(pers.first_names) > 0:
-                st = st + str(pers.first_names[0])
+                st = st + str(pers.first_names[0]).encode('utf-8').decode('utf-8')
             if len(pers.middle_names) > 0:
-                st = st + " "+str(pers.middle_names[0])
+                st = st + " "+str(pers.middle_names[0]).encode('utf-8').decode('utf-8')
             if len(pers.last_names) > 0:
-                st = st + " " +str(pers.last_names[0].encode('utf-8'))
-            st = st + '</span>'            
+                st = st + " " +str(pers.last_names[0].encode('utf-8')).decode('utf-8')
+            st = st + '</span>'    
     if "title" in entry.fields:
-        title = entry.fields["title"]        
+        title = entry.fields["title"].encode('utf-8').decode('utf-8')
     if "author" in entry.fields:
         author = entry.fields["author"]
     if "journal" in entry.fields:
@@ -52,9 +50,18 @@ for key in bib_data.entries:
         venue = entry.fields["howpublished"]            
     elif "publisher" in entry.fields:
         venue = entry.fields["publisher"]
-    print('<p class="bib_entry" id="bib_'+key+'">')
-    print('<span class="bib_title">'+title+'</span>')
-    print(st)
-    print('<span class="bib_venue">'+venue+'</span>')
-    print('</p>')
-    
+    date = "1990-"
+    if "year" in entry.fields:
+        date = str(entry.fields["year"])+"-"
+    if "month" in entry.fields:
+        date = date + str(entry.fields["month"])
+    ast = '<p class="bib_entry" id="bib_'+key+'">'+'<span class="bib_title">'+title+'</span>'+st+'<span class="bib_venue">'+venue+'</span>'+'</p>'
+    if date not in all_bibs:
+        all_bibs[date] = []
+    all_bibs[date].append(ast)
+
+
+for date in sorted(all_bibs.keys(), reverse=True):
+    for bib in all_bibs[date]:
+        print(bib.encode('utf-8'))
+
