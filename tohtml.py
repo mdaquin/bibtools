4afcfb9b5b6b6aa8900e91d81c20d22000c85da9
mdaquin <mathieu@daquin.net>
2021-01-07 11:08:40 +0000

    small change in tohtml

--- tohtml.py
+++ tohtml.py
@@ -63,10 +63,24 @@ for key in bib_data.entries:
         venue = entry.fields["publisher"]
     date = "1990-"
     if "year" in entry.fields:
-        date = str(entry.fields["year"])+"-"
-    if "month" in entry.fields:
-        date = date + str(entry.fields["month"])
-    ast = '<p class="bib_entry" id="bib_'+key+'">'+'<span class="bib_title"><a href="'+url+'">'+title+'</a></span>'+st+'<span class="bib_venue">'+venue+'</span>'+'</p>'
+        date = str(entry.fields["year"])
+    more = ""
+    if "volume" in entry.fields:
+        more = entry.fields["volume"]
+    if "number" in entry.fields:
+        if "volume" in entry.fields:
+            more += ":"
+        more += entry.fields["number"]
+    if "publisher" in entry.fields:
+        if more != "":
+            more += ", "
+        more += entry.fields["publisher"]
+    if "more" != "":
+        more = ", "+more
+    #if "month" in entry.fields:
+    #    date = date + str(entry.fields["month"])
+    # ast = '<p class="bib_entry" id="bib_'+key+'">'+'<span class="bib_title"><a href="'+url+'">'+title+'</a></span>'+st+'<span class="bib_venue">'+venue+'</span>'+'</p>'
+    ast = '<div style="margin-bottom:10px">'+st+" ("+date+") <i>"+title+"</i>, In "+venue+more+"</div>"
     if date not in all_bibs:
         all_bibs[date] = []
     all_bibs[date].append(ast)
